e05b097b77e8f44a15a9a1ef6be8277bab936445
samkimpepper <vegielcl@gmail.com>
2022-04-11 22:39:57 +0900

    MariaDB connected

---
+++ src/main/java/com/turi/turi0411/config/DBConfig.java
@@ -0,0 +1,9 @@
+package com.turi.turi0411.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class DBConfig {
+}
