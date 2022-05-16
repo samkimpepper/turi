9d1cfddd9d79adfc515d3dd44e26ece9c9795286
samkimpepper <vegielcl@gmail.com>
2022-05-16 23:14:15 +0900

    please

--- src/main/java/com/turi/turi0411/Turi0411Application.java
+++ src/main/java/com/turi/turi0411/Turi0411Application.java
@@ -2,10 +2,12 @@ package com.turi.turi0411;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
+@EnableJpaAuditing
 public class Turi0411Application {
 
     public static void main(String[] args) {
